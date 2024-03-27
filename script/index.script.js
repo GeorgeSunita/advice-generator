e309137a47a1289ec8efff670533fb577945c1b0
GeorgeSunita <35330816+GeorgeSunita@users.noreply.github.com>
2024-03-27 10:18:43 +0100

    script

---
+++ script/index.script.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  const adviceDiv = document.getElementById("advice");
+  const formEl = document.getElementById("submit");
+  const adviceHeadingEl = document.getElementById("heading");
+  const displayAdvice = function (advice) {
+    adviceHeadingEl.textContent = advice.slip.id;
+    adviceDiv.innerText = '"' + advice.slip.advice + '"';
+    adviceDiv.classList.add("adDiv");
+  };
+
+  formEl.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then(function (data) {
+        console.log(data.slip);
+        displayAdvice(data);
+      });
+  });
+};
